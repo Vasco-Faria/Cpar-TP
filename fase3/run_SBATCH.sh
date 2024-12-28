7b5415b204949ef1aa080f2d80f0f10808e9aaaa
Duarte Leitão <92164696+Duarte0903@users.noreply.github.com>
2024-12-28 00:11:15 +0000

    testes na partição day

--- fase3/run_SBATCH.sh
+++ fase3/run_SBATCH.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=fluid_sim
-#SBATCH --partition=cpar
+#SBATCH --partition=day
 #SBATCH --exclusive
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20        
 #SBATCH --output=fluid_sim_output.txt
-#SBATCH --time=02:00
+#SBATCH --constraint=k20
 
 # Load any necessary modules (if required)
-module load gcc/11.2.0
+module load gcc/7.2.0
 module load cuda/11.3.1
 
 make > /dev/null 2>&1
 
 # run app
-srun --partition=cpar --exclusive perf stat -r 3 -M cpi,instructions -e branch-misses,L1-dcache-loads,L1-dcache-load-misses,cycles,duration_time,mem-loads,mem-stores ./fluid_sim_cuda
+srun --partition=day --exclusive perf stat -r 1 -M cpi,instructions -e branch-misses,L1-dcache-loads,L1-dcache-load-misses,cycles,duration_time,mem-loads,mem-stores ./fluid_sim_cuda
