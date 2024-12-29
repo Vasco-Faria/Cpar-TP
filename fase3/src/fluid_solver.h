e942c163448869f292f897586bbd7884baff6347
Duarte Leitão <92164696+Duarte0903@users.noreply.github.com>
2024-12-29 18:01:07 +0000

    melhorias na gestão de memória

--- fase3/src/fluid_solver.h
+++ fase3/src/fluid_solver.h
@@ -1,9 +1,14 @@
 #ifndef FLUID_SOLVER_H
 #define FLUID_SOLVER_H
 
-void dens_step(int M, int N, int O, float *x, float *x0, float *u, float *v,
-               float *w, float diff, float dt);
-void vel_step(int M, int N, int O, float *u, float *v, float *w, float *u0,
-              float *v0, float *w0, float visc, float dt);
+void cpy_host_to_device(float* u, float* v, float* w, float* dens);
+void cpy_device_to_host(float* u, float* v, float* w, float* dens);
+void init_cuda_mallocs_vel(int M, int N, int O, float* u, float* v, float* w, float* u0, float* v0, float* w0);
+void free_cuda_mallocs_vel(float* u, float* v, float* w, float* u0, float* v0, float* w0);
+void init_cuda_mallocs_dens(float* x, float* x0);
+void free_cuda_mallocs_dens(float* x, float* x0, float* u, float* v, float* w);
+
+void dens_step(int M, int N, int O, float *x, float *x0, float *u, float *v, float *w, float diff, float dt);
+void vel_step(int M, int N, int O, float *u, float *v, float *w, float *u0, float *v0, float *w0, float visc, float dt);
 
 #endif // FLUID_SOLVER_H
