caea4ab1041cc96f2796d67e9c2956f2e2cb08e6
Duarte Leitão <92164696+Duarte0903@users.noreply.github.com>
2024-12-29 20:01:09 +0000

    utilização de extern

--- fase3/src/fluid_solver.h
+++ fase3/src/fluid_solver.h
@@ -1,12 +1,16 @@
 #ifndef FLUID_SOLVER_H
 #define FLUID_SOLVER_H
 
-void cpy_host_to_device(float* u, float* v, float* w, float* dens);
-void cpy_device_to_host(float* u, float* v, float* w, float* dens);
+extern float* v_u;
+extern float* v_v;
+extern float* v_w;
+
+extern float* d_x;
+
 void init_cuda_mallocs_vel(int M, int N, int O, float* u, float* v, float* w, float* u0, float* v0, float* w0);
-void free_cuda_mallocs_vel(float* u, float* v, float* w, float* u0, float* v0, float* w0);
+void free_cuda_mallocs_vel();
 void init_cuda_mallocs_dens(float* x, float* x0);
-void free_cuda_mallocs_dens(float* x, float* x0, float* u, float* v, float* w);
+void free_cuda_mallocs_dens();
 
 void dens_step(int M, int N, int O, float *x, float *x0, float *u, float *v, float *w, float diff, float dt);
 void vel_step(int M, int N, int O, float *u, float *v, float *w, float *u0, float *v0, float *w0, float visc, float dt);
